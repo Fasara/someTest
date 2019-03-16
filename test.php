29f1f7691dea5d5fa859e21c118e9c91f0c5a4c5
Fabiana <fabiana.asara@gmail.com>
2019-03-16 22:12:52 +0000

    Added some more functionality and style

--- test.php
+++ test.php
@@ -1,76 +1,172 @@
+<?php
+	ini_set('display_errors', 1);
+	error_reporting(E_ALL);
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<title></title>
 </head>
 <body>
-	<h1>Event Registration Form</h1>
-	<div id="container">
-		<form method="POST" action="/someTest/test.php">
-			<ul>
-				<li>
-					<label for="name">Start Number</label>
-					<input type="number" name="start_number" required>
-					<span>Enter the start number</span>	
-				</li>
-				<li>
-					<label for="name">End Number</label>
-					<input type="number" name="end_number" required>
-					<span>Enter the end number</span>	
-				</li>
-				<li>
-					<label for="name">Word for divisible by 3</label>
-					<input type="text" name="by_3">
-					<span></span>	
-				</li>
-		</ul>
-		<button type="submit" class="register">Go</button>
-	</form>
-</div>
+	<?php
 
+	//set the variables to the input element
+		if (isset($_POST["start_number"])) {
 
-
-<div id="exerciseholder">
-
-	<?php 
-
-	$numStart = (int) $_POST["start_number"];
-	$numEnd = (int) $_POST["end_number"];
-
-	$divisibleBy3Word = $_POST["by_3"];
-
-	echo "Start number is " . $numStart;
-	echo "<br/>";
-	echo "End number is " . $numEnd;
-	echo "<br/>";
-
-	$numbers = range($numStart, $numEnd);
-
-	foreach ($numbers as $number) {
-		if ($number % 3 == 0 && $number % 5 == 0) {
-
-			echo "hello";
-
-		} elseif ($number % 3 == 0) {
-
-			echo $divisibleBy3Word;
-
-		} elseif ($number % 5 == 0) {
-			echo "night";
+			$numStart = (int) $_POST["start_number"]; 
 
 		} else {
 
-			echo $number;
+			$numStart = 1;
+		} 
+
+		if(isset($_POST["end_number"]))	{
+
+			$numEnd = (int) $_POST["end_number"];
+
+		} else {
+
+			$numEnd = 100;
+		}
+
+		if (isset($_POST["divisible_by_3_number"])) {
+
+			$divisibleBy3Num = (int) $_POST["divisible_by_3_number"];
+
+		} else {
+
+			$divisibleBy3Num = 3;
 
 		}
-		echo  ", <br>";
 
-	}
+		if(isset($_POST["divisible_by_5_number"])) {
+
+			$divisibleBy5Num = (int) $_POST["divisible_by_5_number"];
+
+		} else {
+
+			$divisibleBy5Num = 5;
+
+		}
+
+		//print words by default 
+
+		if(isset($_POST["by_3"])) {
+
+			$divisibleBy3Word = $_POST["by_3"];
+
+		} else {
+
+			$divisibleBy3Word = "Fizz";
+		}
+
+		if(isset($_POST["by_5"])) {
+
+			$divisibleBy5Word = $_POST["by_5"];
+
+		} else {
+
+			$divisibleBy5Word = "Buzz";
+		}
+
+
+		 if(isset($_POST["by_3and_5"])) {
+
+			$divisibleBy3and5Word = $_POST["by_3and_5"];
+
+		} else {
+
+			$divisibleBy3and5Word = "FizzBuzz";
+
+		}
+
 
 	?>
-</div>
+	<h1>Fizz Buzz</h1>
+	<div id="container">
+		<form method="POST" action="/someTest/test.php">
+			<div class="row">
+				<div class="col-sm-6 col-md-4 col-lg-2 form-input">
+					<label for="start_number">Start Number</label>
+					<input type="number" name="start_number" value="<?= $numStart; ?>">
+					<span>Enter the start number</span>
+				</div>	
+				<div class="col-sm-6 col-md-4 col-lg-2  form-input">
+					<label for="name">End Number</label>
+					<input type="number" name="end_number" value="<?= $numEnd; ?>">
+					<span>Enter the end number</span>	
+				</div>
+				<div class="col-sm-6 col-md-4 col-lg-2  form-input">
+					<label for="name">Word for divisible by 3</label>
+					<input type="text" name="by_3" value="<?= $divisibleBy3Word; ?>">
+					<span>Enter a word</span>	
+				</div>
+				<div class="col-sm-6 col-md-4 col-lg-2  form-input">
+					<label for="name">Word for divisible by 5</label>
+					<input type="text" name="by_5" value="<?= $divisibleBy5Word; ?>">
+					<span>Enter a word</span>
+				</div>
+				<div class="col-sm-6 col-md-4 col-lg-2  form-input">
+					<label for="name">Word for divisible by 3 and 5</label>
+					<input type="text" name="by_3and_5" value="<?= $divisibleBy3and5Word; ?>">
+					<span>Enter a word</span>
+				</div>
+				<div class="col-sm-6 col-md-4 col-lg-2  form-input">
+					<label for="name">Divisible by 3 number</label>
+					<input type="number" name="divisible_by_3_number" value="<?= $divisibleBy3Num; ?>">
+					<span>Enter a number</span>
+				</div>
+				<div class="col-sm-6 col-md-4 col-lg-2  form-input">
+					<label for="name">Divisible by 5 number</label>
+					<input type="number" name="divisible_by_5_number" value="<?= $divisibleBy5Num; ?>">
+					<span>Enter a number</span>
+				</div>
+			</div>
+			<button type="submit" class="btn btn-primary btn-lg btn-go">Go</button>
+		</form>
+	</div>
 
+
+
+	<div id="exerciseholder">
+
+		<?php 
+		
+
+		echo "Start number is " . $numStart;
+		echo "<br/>";
+		echo "End number is " . $numEnd;
+		echo "<br/>";
+
+		$numbers = range($numStart, $numEnd);
+
+		foreach ($numbers as $number) {
+
+			if ($number % $divisibleBy3Num == 0 && $number % $divisibleBy5Num == 0) {
+
+				echo $divisibleBy3and5Word;
+
+			} elseif ($number % $divisibleBy3Num == 0) {
+
+				echo $divisibleBy3Word; 
+
+			} elseif ($number % $divisibleBy5Num == 0) {
+				echo $divisibleBy5Word;
+
+			} else {
+
+				echo $number;
+
+			}
+			echo  ", <br>";
+
+		}
+
+		?>
+	</div>
 </body>
 </html>
 
